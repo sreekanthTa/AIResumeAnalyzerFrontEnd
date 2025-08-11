38eb41dea0eb153ca61d49737a50a8e27ce48640
sreekanthTa <sreekanthsreekantht970@gmail.com>
2025-08-11 13:55:59 +0530

    fixes

--- src/pages/CodingEditorPage/index.jsx
+++ src/pages/CodingEditorPage/index.jsx
@@ -101,21 +101,33 @@ const CodingEditorPage = () => {
   return (
     <div className={styles["coding-editor-page"]}>
       <div className={styles["problem-container"]}>
-        <h1 className={styles["problem-title"]}>{problem.title}</h1>
-        <p className={styles["problem-difficulty"]}>
-          Difficulty: <span>{problem.difficulty}</span>
-        </p>
-        <p className={styles["problem-date"]}>
-          Created At: <span>{new Date(problem.created_at).toLocaleString()}</span>
-        </p>
-        <p className={styles["problem-date"]}>
-          Updated At: <span>{new Date(problem.updated_at).toLocaleString()}</span>
-        </p>
-        <h2 className={styles["problem-question"]}>Question</h2>
-        <p>{problem.question}</p>
-        <br/>
-        <h2 className={styles["problem-description"]}>Question</h2>
-        <p>{problem.description}</p>
+        <header>
+          <h1 className={styles["problem-title"]}>{problem.title}</h1>
+          <div className={styles["problem-metadata"]}>
+            <p className={styles["problem-difficulty"]}>
+              <span>{problem.difficulty}</span>
+            </p>
+            <div className={styles["problem-dates"]}>
+              <p className={styles["problem-date"]}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M5.75 2a.75.75 0 01.75.75V4h7V2.75a.75.75 0 011.5 0V4h.25A2.75 2.75 0 0118 6.75v8.5A2.75 2.75 0 0115.25 18H4.75A2.75 2.75 0 012 15.25v-8.5A2.75 2.75 0 014.75 4H5V2.75A.75.75 0 015.75 2zm-1 5.5c-.69 0-1.25.56-1.25 1.25v6.5c0 .69.56 1.25 1.25 1.25h10.5c.69 0 1.25-.56 1.25-1.25v-6.5c0-.69-.56-1.25-1.25-1.25H4.75z" clipRule="evenodd" />
+                </svg>
+                <span>{new Date(problem.created_at).toLocaleDateString()}</span>
+              </p>
+              <p className={styles["problem-date"]}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm.75-13a.75.75 0 00-1.5 0v5c0 .414.336.75.75.75h4a.75.75 0 000-1.5h-3.25V5z" clipRule="evenodd" />
+                </svg>
+                <span>{new Date(problem.updated_at).toLocaleDateString()}</span>
+              </p>
+            </div>
+          </div>
+        </header>
+
+        <div className={styles["problem-section"]}>
+          <h2 className={styles["problem-description"]}>Problem Description</h2>
+          <p>{problem.description}</p>
+        </div>
 
     
         <h2 className={styles["problem-sample"]}>Sample Input/Output</h2>
